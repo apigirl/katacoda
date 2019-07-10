e825933507bbcc8b21db5cbf210510f47104099a
Gregory <gregory.zussa@datadoghq.com>
2019-07-10 18:51:45 +0200

    update script step 1

--- dd-workshop-dash-2019/scripts/step1.sh
+++ dd-workshop-dash-2019/scripts/step1.sh
@@ -10,18 +10,18 @@ git clone -q git@github.com:gzussa/integrations-extras.git /integrations-extras
 cd /integrations-extras && cd /integrations-extras
 git checkout gzu/gr
 
-#echo "Creating workspaces..." | wall -n
+echo "Creating workspaces..." | wall -n
 
-#mkdir /workspace && cd /workspace
-#mkdir /workspace/solution
-#
-#echo "Cloning the integrations-extras repository..." | wall -n
-#git clone -q git@github.com:DataDog/integrations-extras.git /workspace/integration-extras
-#
-#echo "Setup a python3 virtual environment..." | wall -n
-#cd /workspace/integration-extras
-#virtualenv venv -p python3
-#source venv/bin/active
+mkdir /workspace && cd /workspace
+mkdir /workspace/solution
+
+echo "Cloning the integrations-extras repository..." | wall -n
+git clone -q git@github.com:DataDog/integrations-extras.git /workspace/integration-extras
+
+echo "Setup a python3 virtual environment..." | wall -n
+cd /workspace/integration-extras
+virtualenv venv -p python3
+source venv/bin/active
 
 #echo "Install and configure the datadog-check-dev cli..." | wall -n
 #pip install "datadog-checks-dev[cli]"
